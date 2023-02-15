6cab399317529ca6a38ca2b711f32eba737b78d4
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-15 09:34:38 +0100

    bonus feature built, thankfully

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  function emojiFunction(minutes) {
+    let emojiArray = [];
+
+    if (minutes < 30) {
+      for (let i = 0; i < Math.ceil(minutes / 5); i++)
+        emojiArray.push(<span key={i}>☕️</span>);
+    } else {
+      for (let i = 0; i < Math.ceil(minutes / 10); i++)
+        emojiArray.push(<span key={i}>🍱</span>);
+    }
+
+    return emojiArray;
+  }
+
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
+      <small> &bull; </small>
+      <small>
+        {emojiFunction(minutes)} {minutes} min read
+      </small>
       <p>{preview}</p>
     </article>
   );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,14 +2,15 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({ posts }) {
-  console.log("ArticleList", posts);
-  
+  // console.log("ArticleList", posts);
+
   const articlesList = posts.map((member) => (
     <Article
       key={member.id}
       title={member.title}
       date={member.date}
       preview={member.preview}
+      minutes={member.minutes}
     />
   ));
 
