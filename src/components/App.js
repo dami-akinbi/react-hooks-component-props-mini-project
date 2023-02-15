41bf7aacb25cf5f10e2944894c2affd4d05c464d
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-15 08:40:51 +0100

    About component built, rendered on browser

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import blogData from "../data/blog";
 import Header from "./Header";
+import About from "./About";
 
 console.log("App", blogData);
 
@@ -11,6 +12,7 @@ function App() {
       {/* You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck! */}
       <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
     </div>
   );
 }
